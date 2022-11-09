7951394e40521e332dbe26654d95d5e4dd4f2914
xjoxz <104948498+xjoxz@users.noreply.github.com>
2022-11-09 10:52:23 -0500

    Se ha subido el archivo Bash 1
    Josué Leonardo Chango Gómez ha realizado esta modificación

---
+++ tc2_ChangoJosue.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "\n"
+echo "Comando para escoger las primeras 7 filas"
+echo -e "\n"
+head -n 7 EigenBetw.tsv
+echo -e "\n"
+echo "Comando para escoger las últimas 5 filas"
+tail -n 5 EigenBetw.tsv
+
